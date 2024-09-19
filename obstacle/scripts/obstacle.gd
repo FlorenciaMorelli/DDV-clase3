d7d5c54e31eee2b20967d6f687911c8773cd5837
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-09-18 21:49:27 -0300

    Base Position of the Trigger set

--- obstacle/scripts/obstacle.gd
+++ obstacle/scripts/obstacle.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+#	var -> variables
+#	export permite ver la variable en el inspector del nodo
+@export var basePosition = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if basePosition != Vector2(0, 0) :
+		$Trigger.position = basePosition
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
